f8139c822577043b44f049097cdba06178054aa2
Philip Lee-Chin <phil.leechin@outlook.com>
2020-04-22 21:51:04 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,22 @@
 class Pokemon
+  attr_accessor :name, :type, :db, :id
+  #attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = "INSERT INTO pokemon (name, type) VALUES (?,?)"
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = (?);"
+    poke = db.execute(sql, [id]).flatten
+    Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: poke[3])
+  end
 end
